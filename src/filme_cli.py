9a172356d43d932c890a1c9b48de52febfbc0f1e
Afonso Neto <afonsoneto121@gmail.com>
2021-07-10 18:26:27 -0300

    Corringo bug nome

--- src/filme_cli.py
+++ src/filme_cli.py
@@ -3,14 +3,15 @@ import src.main as main
 
 def init():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', help='Entre com o titulo de um filme ou série' )
+    parser.add_argument('-f', help='Entre com o titulo de um filme ou série',nargs='+',metavar='N' )
     parser.add_argument('-d', help='Ver Top 10 em destaque',action="store_true")
     parser.add_argument('--token', help='Ver Top 10 em destaque')
 
 
     args = parser.parse_args()
     if args.f:
-        main.get_movie(args.f)
+        str = ' '.join(args.f)
+        main.get_movie(str)
         pass
     elif args.d:
         main.get_trending()
